cc9183af4bfc361a36cef2227147b30f37e5d46b
April Prichard <fuzzygamebear@yahoo.com>
2022-06-05 10:52:50 -0700

    Tested on real repository

--- src/ImportRefactorer.ts
+++ src/ImportRefactorer.ts
@@ -2,53 +2,72 @@ import chalk from 'chalk';
 import fs from 'fs'
 import { glob } from 'glob';
 import path, { basename } from "path";
-import transformImports from "transform-imports";
+import { SourceFile, SyntaxKind } from 'ts-morph';
 
 export default class ImportRefactorer
 {
-    public static rewriteImports(code: string, filepath: string, possibleimports: string[]): string
+    public static async rewriteImports(sourceFile: SourceFile, filepath: string, possibleImports: string[]): Promise<SourceFile>
     {
-        console.log(chalk.blue(`${filepath}`))
-        const newCode = transformImports(code, (importDefs) =>
+        console.log(chalk.blue(`${filepath}`))        
+
+        const nodeNames: Set<string> = new Set();
+        const importDeclarations = sourceFile.getChildrenOfKind(SyntaxKind.ImportDeclaration)
+
+        for(const importDeclaration of importDeclarations)
         {
-            importDefs.forEach((importDef) =>
+            const importClause = importDeclaration.asKind(SyntaxKind.ImportDeclaration)?.getImportClause();
+            const moduleSpecifier = importDeclaration.asKind(SyntaxKind.ImportDeclaration)?.getModuleSpecifier();
+
+            console.log(`import ${importClause!.getFullText()} from ${moduleSpecifier!.getFullText()}`);
+            if(!importClause)
             {
+                console.log(`importClause is undefined`);
+                continue
+            }
 
-                if (ImportRefactorer.isRelativeImport(importDef.source))
+            if(!moduleSpecifier)
+            {
+                console.log(`moduleSpecifier is undefined`);
+                continue;
+            }
+
+            const cleanedModuleSpecifier = moduleSpecifier.getFullText().trim().replace(/["']/g, "");
+            if (ImportRefactorer.isRelativeImport(cleanedModuleSpecifier))
+            {
+                const possiblePaths = ImportRefactorer.listPossiblePaths(filepath, cleanedModuleSpecifier);
+                let found = false;
+                for (const path of possiblePaths)
                 {
-                    const possiblePaths = ImportRefactorer.listPossiblePaths(filepath, importDef.source);
-                    let found = false;
-                    for (const path of possiblePaths)
+                    if (await ImportRefactorer.isFile(path))
                     {
-                        if (ImportRefactorer.isFile(path))
-                        {
-                            found = true
-                            break
-                        }
+                        found = true
+                        break
                     }
-
-                    if (!found)
-                    {
-                        const updatedPath = ImportRefactorer.lookForMatchingFile(filepath, importDef.source, possibleimports);
-                        if (!updatedPath)
-                        {
-                            console.log(chalk.red(`    "${importDef.source}" No match found`))
-                        }
-                        else
-                        {
-                            console.log(chalk.green(`    "${importDef.source}" -> "${updatedPath}"`))
-                            importDef.source = updatedPath;
-                        }
-                    }
-
                 }
-            });
-        });
 
-        return newCode;
+                if (!found)
+                {
+                    const updatedPath = ImportRefactorer.lookForMatchingFile(filepath, cleanedModuleSpecifier, possibleImports);
+
+                    if (!updatedPath)
+                    {
+                        console.log(chalk.red(`    "${cleanedModuleSpecifier}" No match found`))
+                    }
+                    else
+                    {
+                        const correctedSlashesPath = updatedPath.replace(/[\\]/g, "/")
+                        const removedTypeScriptExtensionPath = correctedSlashesPath.replace(/\.tsx?$/, "")
+                        console.log(chalk.green(`    "${cleanedModuleSpecifier}" -> "${removedTypeScriptExtensionPath}"`))
+                        moduleSpecifier.replaceWithText(` '${removedTypeScriptExtensionPath}'`);
+                    }
+                }
+            }
+        }
+
+        return sourceFile;
     }
 
-    private static lookForMatchingFile(filepath: string, source: string, possibleimports: string[]): string | null
+    private static lookForMatchingFile(filepath: string, source: string, possibleImports: string[]): string | null
     {
         const cleanedSource = ImportRefactorer.cleanSource(source)
 
@@ -57,7 +76,7 @@ export default class ImportRefactorer
         {
             const basename = path.basename(cleanedSource)
 
-            for (const possibleImport of possibleimports)
+            for (const possibleImport of possibleImports)
             {
                 if (path.basename(possibleImport) === basename)
                 {
@@ -67,7 +86,7 @@ export default class ImportRefactorer
         }
         else
         {
-            const matches = /[\/\\]([a-zA-Z0-9_-])$/.exec(cleanedSource)
+            const matches = /[\/\\]([a-zA-Z0-9_-]+)$/.exec(cleanedSource)
             if (!matches)
             {
                 throw new Error(`source ${cleanedSource} from file ${filepath} did not match basename extraction regex.`)
@@ -75,29 +94,39 @@ export default class ImportRefactorer
 
             const basename = matches[1];
 
-            for (const possibleImport of possibleimports)
+            for (const possibleImport of possibleImports)
             {
-                if (ImportRefactorer.trimFileExtension(path.basename(possibleImport)) === basename)
+                const trimmedPossibleImport = ImportRefactorer.trimFileExtension(path.basename(possibleImport))
+                if (trimmedPossibleImport === basename)
                 {
                     matchingImports.push(possibleImport)
+                }
+                else if(["index.ts", "index.tsx"].includes(path.basename(possibleImport)))
+                {
+                    if(path.basename(path.dirname(possibleImport)) === basename)
+                    {
+                        matchingImports.push(possibleImport);
+                    }
                 }
             }
         }
 
+        let result = ''
         if (matchingImports.length === 0)
         {
             return null;
         }
         else if (matchingImports.length === 1)
         {
-            return matchingImports[0];
+            result = path.relative(path.join(filepath, ".."), matchingImports[0]);
+
         }
         else
         {
             matchingImports.sort((a, b) =>
             {
-                const aRel = path.relative(filepath, a);
-                const bRel = path.relative(filepath, b);
+                const aRel = path.relative(path.join(filepath, ".."), a);
+                const bRel = path.relative(path.join(filepath, ".."), b);
 
                 if (aRel > bRel)
                 {
@@ -113,8 +142,15 @@ export default class ImportRefactorer
                 }
             })
 
-            return matchingImports[0]
+            result = path.relative(path.join(filepath, ".."), matchingImports[0])
         }
+
+        if(!result.startsWith('.'))
+        {
+            result = `./${result}`;
+        }
+
+        return result
 
     }
 
@@ -136,7 +172,7 @@ export default class ImportRefactorer
 
     public static async asyncGlob(pattern: string): Promise<string[]>
     {
-        return new Promise<string[]>((res, rej) => glob(pattern, (error, matches) =>
+        return new Promise<string[]>((res, rej) => glob(pattern, {absolute: true}, (error, matches) =>
         {
             if (error)
             {
@@ -159,7 +195,7 @@ export default class ImportRefactorer
 
     private static listPossiblePaths(fileLocation: string, source: string)
     {
-        const pathOne = path.join(fileLocation, source)
+        const pathOne = path.join(path.join(fileLocation, ".."), source)
 
         if (ImportRefactorer.hasFileExtension(fileLocation))
         {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import chalk from 'chalk';
 import fs from 'fs';
 import yargs from 'yargs';
 import ImportRefactorer from './ImportRefactorer';
+import { Project, ScriptTarget } from "ts-morph";
 
 /**
  * "source": is a glob that should match the files that have bad imports
@@ -15,6 +17,10 @@ const options = yargs
         alias: 'i',
         demandOption: true
     })
+    .option('ts-config', {
+        alias: 't',
+        demandOption: true
+    })
     .option('dry-run', {
         alias: 'd',
         boolean: true,
@@ -23,36 +29,67 @@ const options = yargs
     .argv;
 
 
-const fufilledOptions = await options;
-
-const sourceCodeFilepaths = await ImportRefactorer.asyncGlob(fufilledOptions.source as string);
-const possibleImports = await ImportRefactorer.asyncGlob(fufilledOptions.imports as string);
-
-console.log(`Found ${sourceCodeFilepaths.length} source code files`);
-console.log(`Found ${possibleImports.length} possible imports`);
-
-for(const sourceCodeFilepath in sourceCodeFilepaths)
+(async () =>
 {
-    const sourceCode = await readFile(sourceCodeFilepath);
-    const newcode = ImportRefactorer.rewriteImports(sourceCode, sourceCodeFilepath, possibleImports);
+    const fulfilledOptions = await options;
 
-    if(!fufilledOptions['dry-run'] && newcode !== sourceCode)
+    const sourceCodeFilepaths = await ImportRefactorer.asyncGlob(fulfilledOptions.source as string);
+    const possibleImports = await ImportRefactorer.asyncGlob(fulfilledOptions.imports as string);
+
+    const project = new Project({
+        tsConfigFilePath: fulfilledOptions['ts-config'] as string,
+        //skipAddingFilesFromTsConfig: true,
+    });
+
+    console.log(`Found ${sourceCodeFilepaths.length} source code files`);
+    console.log(`Found ${possibleImports.length} possible imports`);
+
+    const sourceFiles = project.getSourceFiles();
+    console.log(sourceFiles.length)
+
+    for (const sourceCodeFilepath of sourceCodeFilepaths)
     {
-        await writeFile(sourceCodeFilepath, newcode);
-        console.log(`Wrote out new code to ${sourceCodeFilepath}`);
+        const fileContents = await readFile(sourceCodeFilepath)
+        const sourceCode = project.createSourceFile(sourceCodeFilepath, fileContents, {overwrite: true});
+        //const sourceCode = project.getSourceFile(sourceCodeFilepath);
+
+        if (!sourceCode)
+        {
+            console.log(chalk.red(`Could not find file "${sourceCodeFilepath}"`));
+            continue;
+        }
+
+        try 
+        {
+            const newCode = await ImportRefactorer.rewriteImports(sourceCode, sourceCodeFilepath, possibleImports);
+
+            if (!fulfilledOptions['dry-run'])
+            {
+                await newCode.save()
+                console.log(`Wrote out new code to ${sourceCodeFilepath}`);
+            }
+        }
+        catch (ex)
+        {
+            console.log(chalk.red(`Could not process file "${sourceCodeFilepath}"`))
+            console.log(ex)
+        }
     }
-}
 
-async function readFile(filepath: string): Promise<string> 
-{
-    const data = await fs.promises.readFile(filepath, 'utf8');
-    return data;
-}
+    async function readFile(filepath: string): Promise<string> 
+    {
+        console.log(`Opening file "${filepath}"`)
+        const data = await fs.promises.readFile(filepath, 'utf8');
+        return data;
+    }
 
-async function writeFile(filepath: string, contents: string): Promise<void> 
-{
-    await fs.promises.writeFile(filepath, contents, 'utf8');
-}
+    async function writeFile(filepath: string, contents: string): Promise<void> 
+    {
+        await fs.promises.writeFile(filepath, contents, 'utf8');
+    }
+
+})()
+
 
 
 
